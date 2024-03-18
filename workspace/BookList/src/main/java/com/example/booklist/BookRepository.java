572f00a773d67081d36fb58a9f21c9bcc8cad158
saxit0104 <163510245+saxit0104@users.noreply.github.com>
2024-03-18 22:17:34 +0900

    Delete workspace directory

--- workspace/BookList/src/main/java/com/example/booklist/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.booklist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
--- workspace/BookList/src/main/java/com/example/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // ユーザー名でユーザーを探すメソッド。ユーザー名をパラメータとして渡すと、そのユーザー名を持つユーザーをデータベースから探して返します。
-	User findByUsername(String username);
-}
